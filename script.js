cb26c836192fa695724c1a1a80f55d0ecf250ca5
R-K-96 <124651427+R-K-96@users.noreply.github.com>
2023-05-18 19:20:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 //your code here
+
+function calculateTotalPrice() {
+      var prices = document.querySelectorAll('[data-ns-test="prices"]');
+      var total = 0;
+
+      // Iterate through each price element and sum the values
+      prices.forEach(function(priceElement) {
+        var price = parseFloat(priceElement.textContent);
+        if (!isNaN(price)) {
+          total += price;
+        }
+      });
+
+      // Create a new row for displaying the total price
+      var table = document.getElementById('groceryTable');
+      var newRow = document.createElement('tr');
+      var totalCell = document.createElement('td');
+      totalCell.setAttribute('colspan', '2');
+      totalCell.textContent = 'Total Price: ' + total.toFixed(2);
+      newRow.appendChild(totalCell);
+
+      // Set the data attribute for identifying the total element
+      totalCell.setAttribute('data-ns-test', 'grandTotal');
+
+      // Append the new row to the table
+      table.appendChild(newRow);
+    }
+
+    // Calculate the total price initially
+    calculateTotalPrice();
